f5c571477a6a939ef40c7f44f8e99f4f90c81fde
Demo-Repos <khushrajrai@gmail.com>
2026-04-01 02:18:01 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
                 Khushraj Rai © {new Date().getFullYear()}
             </span>
             <span >
-                <img src="public/favicon.png" style={{ padding: '3px 1px 0px 1px', width: "30px", height: "30px", alignItems: 'center', justifyContent: 'center' }}
+                <img src="favicon.png" style={{ padding: '3px 1px 0px 1px', width: "30px", height: "30px", alignItems: 'center', justifyContent: 'center' }}
                     alt="logo" />
             </span>
             <div style={{ textAlign: 'right' }}>
